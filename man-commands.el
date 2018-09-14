060a2ad07c9740df570e9e80b03cfa03853ac5c4
Reuben Thomas <rrt@sc3d.org>
2018-09-14 21:01:06 +0100

    Fix typo

--- man-commands.el
+++ man-commands.el
@@ -12,7 +12,7 @@
 ;;; Commentary:
 
 ;; I usually try to do M-x man-mail, or whatever command I'm trying, before
-;; going to M-x max and entering the command.  The following actually installs
+;; going to M-x man and entering the command.  The following actually installs
 ;; all of the commands you can man as interactive command.
 
 ;;; Installation:
